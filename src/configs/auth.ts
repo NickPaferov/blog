a363506e39d219739c4fef9f59f5d6fbb94e891e
NickPaferov <paferov.nikolai@gmail.com>
2023-10-12 23:36:30 +0300

    Fix types and credentials in providers (auth.ts)

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,24 +1,24 @@
-import type { NextAuthOptions } from 'next-auth';
+import type { NextAuthOptions, User } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { TEST_USER } from '@/constants/testUser';
 
-//@ts-ignore
 export const authConfig: NextAuthOptions = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
-        email: { label: 'email', type: 'email', placeholder: 'test@gmail.com', required: true },
-        password: { label: 'password', type: 'text', placeholder: '0123456789', required: true },
+        email: { label: 'email', type: 'email', placeholder: 'test@gmail.com' },
+        password: { label: 'password', type: 'text', placeholder: '0123456789' },
       },
       async authorize(credentials) {
         if (!credentials.email || !credentials.password) return null;
-        if (credentials.email === TEST_USER.email && credentials.password === TEST_USER.password) return TEST_USER;
+        if (credentials.email === TEST_USER.email && credentials.password === TEST_USER.password)
+          return TEST_USER as User;
         return null;
       },
     }),
